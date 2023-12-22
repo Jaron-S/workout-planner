fe97a6e7baff865cfe2b9315b00552a9067f6508
Jaron-S <jaronschoor@gmail.com>
2023-12-22 10:48:25 -0700

    favicon fix

--- src/app/_components/navbar/Logo.tsx
+++ src/app/_components/navbar/Logo.tsx
@@ -1,14 +1,7 @@
 import Image from "next/image";
 
 const Logo = () => {
-  return (
-    <Image
-      src={"/program_balancer_logo_light.png"}
-      alt={"Logo"}
-      width={35}
-      height={35}
-    />
-  );
+  return <Image src={"/icon.png"} alt={"Logo"} width={35} height={35} />;
 };
 
 export default Logo;
